4d9fb138f2169672ecb6adf8012c9ff1b3a38494
TheBurn <sunminlei@gmail.com>
2019-03-23 11:34:30 +0800

    init config

---
+++ configure/config.go
@@ -0,0 +1,70 @@
+package configure
+
+import (
+	log "Go-NB-IoT/logging"
+	"encoding/json"
+	"io/ioutil"
+)
+
+/*
+{
+"req_param" : {
+    "cert_file": "certs/server.crt",  # from IoT-platform
+    "key_file": "certs/server.key",   # from IoT-platform
+    "iot_host": "https://180.101.147.89:8743",
+    "app_id": "<APP ID>",
+    "secret": "<SECRET>"
+    },
+"profile_info": {
+    "manufacturer_name": "NAME",
+    "manufacturer_id": "ID",
+    "end_user_id": "END_USER_ID",
+    "location": "Unknown",
+    "device_type": "DEV_TYPE",
+    "device_model": "DEV_MODEL"
+    }
+}
+*/
+
+type ReqParam struct {
+	CertFile string `json:"cert_file"`
+	KeyFile  string `json:"key_file"`
+	IoTHost  string `json:"iot_host"`
+	AppID    string `json:"app_id"`
+	Secret   string `json:"secret"`
+}
+
+type ProfileInfo struct {
+	ManufacturerName string `json:"manufacturer_name"`
+	ManufacturerID   string `json:"manufacturer_id"`
+	EndUserID        string `json:"end_user_id"`
+	Location         string `json:"location"`
+	DeviceType       string `json:"device_type"`
+	DeviceModel      string `json:"device_model"`
+}
+
+type Config struct {
+	ReqParam    ReqParam    `json:"req_param"`
+	ProfileInfo ProfileInfo `json:"profile_info"`
+}
+
+var NBIoTConfig Config
+
+func LoadConfig(configfilename string) error {
+	log.Infof("starting load configure file(%s)......", configfilename)
+	data, err := ioutil.ReadFile(configfilename)
+	if err != nil {
+		log.Errorf("ReadFile %s error:%v", configfilename, err)
+		return err
+	}
+
+	log.Infof("loadconfig: \r\n%s", string(data))
+
+	err = json.Unmarshal(data, &NBIoTConfig)
+	if err != nil {
+		log.Errorf("json.Unmarshal error:%v", err)
+		return err
+	}
+	log.Infof("get config json data:%v", NBIoTConfig)
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"time"
+
+	"Go-NB-IoT/configure"
+	log "Go-NB-IoT/logging"
+)
+
+var (
+	version        = "v1.0"
+	configfilename = flag.String("cfgfile", "conf/config.json", "live configure filename")
+	loglevel       = flag.String("loglevel", "info", "log level")
+	logfile        = flag.String("logfile", "logs/go-nb-iot.log", "log file path")
+)
+
+func init() {
+	flag.Parse()
+	log.SetOutputByName(*logfile)
+	log.SetRotateByDay()
+	log.SetLevelByString(*loglevel)
+}
+
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Error("go-nb-iot panic: ", r)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	err := configure.LoadConfig(*configfilename)
+	if err != nil {
+		return
+	}
+
+	log.SetMaxLogDay(7)
+
+	// output system info
+	log.Info("-----------------START----------------")
+	log.Info("start go-nb-iot: ", version)
+}
