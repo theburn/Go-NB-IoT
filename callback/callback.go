546ed942f5312f1413ada808a4175fa154c25066
TheBurn <sunminlei@gmail.com>
2019-05-03 23:34:48 +0800

    add tow data type

--- callback/callback.go
+++ callback/callback.go
@@ -10,6 +10,18 @@ type DeviceServiceData struct {
 	EventTime   string `json:"eventTime"`
 }
 
+type ConnectivityData struct {
+	SignalPower int `json:"SignalPower "`
+	CellID      int `json:"CellID"`
+	SNR         int `json:"SNR"`
+	Battery     int `json:"battery"`
+}
+
+type LiquidTransmissionData struct {
+	Tank_ID_sign   int `json:"Tank_ID_sign"`
+	Liquid_precent int `json:"liquid_precent"`
+}
+
 type CallbackDeviceDataChanged struct {
 	NotifyType string            `json:"notifyType"`
 	DeviceId   string            `json:"deviceId"`
