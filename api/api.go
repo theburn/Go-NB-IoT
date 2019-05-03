a78b8cac2b1f7fd4b0e76e769b7456afdfff23c5
TheBurn <sunminlei@gmail.com>
2019-05-04 00:57:16 +0800

    update api run args

--- api/api.go
+++ api/api.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"github.com/theburn/Go-NB-IoT/configure"
 	log "github.com/theburn/Go-NB-IoT/logging"
 
 	"github.com/buaazp/fasthttprouter"
@@ -15,11 +14,11 @@ func init() {
 	apiRouter = fasthttprouter.New()
 }
 
-func Run() error {
+func Run(listenPort, staticPath string) error {
 	log.Info("Start HTTP ListenAndServe ... ")
 	// request /static/css/xxx.css -> css/xxx.css
-	fsHandler = fasthttp.FSHandler(configure.NBIoTConfig.ServerParam.StaticPath, 1)
-	ListenPort := ":" + configure.NBIoTConfig.ServerParam.ListenPort
+	fsHandler = fasthttp.FSHandler(staticPath, 1)
+	ListenPort := ":" + listenPort
 
 	apiRouter.POST("/api/callback/v1.5.1/deviceDataChanged", CallBackDeviceDataChanged)
 	apiRouter.GET("/static/*filepath", ServStatic)
